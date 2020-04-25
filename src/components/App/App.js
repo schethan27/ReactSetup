ae99353f70322649baaee5fbc99cf6100d26d173
Chethan S <schethan27@gmail.com>
2020-04-25 12:35:22 +0530

    Added PrimeTable component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React, {Component} from 'react';
+import PrimeTable from './PrimeTable';
 
 class App extends Component {
     render(){
         return <div>
             <h1>My React App</h1>
+            <PrimeTable/>
         </div>;
     }
 }
