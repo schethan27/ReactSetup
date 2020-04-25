62bf8fe825a29ba937039683289e7aa716aeb0a8
Chethan S <schethan27@gmail.com>
2020-04-25 12:27:17 +0530

    Updated Webpack configuration to build scss and other file types

--- webpack.common.js
+++ webpack.common.js
@@ -14,6 +14,22 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.(scss|css)$/,
+                loader: 'style-loader!css-loader!sass-loader'
+            },
+            {
+                test: /\.(png|jpe?g|gif|woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: '[name].[ext]',
+                      outputPath: 'resources/assets'
+                    }
+                  }
+                ]
             }
         ]
     }
