a21e9173d502485685607af6224fd7827e285820
Chethan S <schethan27@gmail.com>
2024-07-11 12:46:50 +0530

    Update PrimeReactTable.js

--- src/components/App/PrimeReactTable.js
+++ src/components/App/PrimeReactTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {DataTable} from 'primereact/datatable/';
+import {DataTable} from 'primereact/datatable'
 import {Column} from 'primereact/column';
 import * as PostsApiService from '../../services/PostsApiService';
 
