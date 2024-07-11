a0829ae17d4b16584ba71a356a810311c07f50be
Chethan S <schethan27@gmail.com>
2024-07-11 12:46:34 +0530

    Update App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
-import PrimeTable from './PrimeTable';
+import PrimeReactTable from './PrimeReactTable';
 
 class App extends Component {
     render(){
         return <div>
             <h1>My React App</h1>
-            <PrimeTable/>
+            <PrimeReactTable/>
         </div>;
     }
 }
